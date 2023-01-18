aa2ca0d905cea6d5eb580b5fbbdaaf9d921f8a42
Rahimi.MO <Rahimi.MO@orchidpharmed.com>
2023-01-18 18:40:05 +0330

    Fix bug in the package

--- src/install.js
+++ src/install.js
@@ -1,9 +1,11 @@
 import HelloWorld from "./components/HelloWorld.vue";
+import Button from "./components/vue-button.vue";
 
 const HelloWorldSimple={
 
     install(Vue){
         Vue.component("hello-world",HelloWorld)
+        Vue.component("vue-button",Button)
     }
 };
 
